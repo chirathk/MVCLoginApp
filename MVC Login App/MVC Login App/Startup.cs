f5413af8a740da5f128c02250c4fe9a82c93b88e
Chirath Kumarasiri <chirathk@gmail.com>
2014-11-22 23:05:38 +0530

    Initial Commit

---
+++ MVC Login App/MVC Login App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Login_App.Startup))]
+namespace MVC_Login_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
